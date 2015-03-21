d2b6d439ad2483f21bbe8c562d11a423af08e15b
Altra Developer Magic <altradev@hotmail.com>
2015-03-20 23:48:16 -0400

    Basic Stuff

--- Craficity/src/com/AltraDev/Crafticity/Crafticity.java
+++ Craficity/src/com/AltraDev/Crafticity/Crafticity.java
@@ -1,6 +1,8 @@
 package com.AltraDev.Crafticity;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,5 +14,11 @@ public class Crafticity extends JavaPlugin implements Listener {
 	
 	public void onDisable() {
 		Bukkit.getServer().getLogger().info("Crafticity has been disabled!");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		
+		
+		return true;
 	}
 }
