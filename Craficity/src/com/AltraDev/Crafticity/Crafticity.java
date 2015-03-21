12ae989aa7650d95283e4ca803c8808d4e4d1591
Altra Developer Magic <altradev@hotmail.com>
2015-03-21 01:15:57 -0400

    Cleaned up code

--- Craficity/src/com/AltraDev/Crafticity/Crafticity.java
+++ Craficity/src/com/AltraDev/Crafticity/Crafticity.java
@@ -25,30 +25,28 @@ public class Crafticity extends JavaPlugin {
 		Player player = (Player) sender;
 		// Start of Poke Command
 		if (cmd.getName().equalsIgnoreCase("poke")) {
-			   if (args.length == 0) {
-				  
-			   sender.sendMessage(ChatColor.GOLD + "Use this command to poke someone!");
-			   }
+			if (args.length == 0) {
+				sender.sendMessage(ChatColor.GOLD + "Use this command to poke someone!");
+			}
 			   
-			   @SuppressWarnings("deprecation")
+			@SuppressWarnings("deprecation")
 			Player target = Bukkit.getServer().getPlayer(args[0]);
-			   
-			   if (target == null) {
-			    sender.sendMessage(ChatColor.GOLD + "Could not find player");
-			    return true;
-			   }
-			   if (target == sender) {
-				   sender.sendMessage(ChatColor.RED + "You can not poke yourself!");
-				   return true;
-			   }
-			   sender.sendMessage(ChatColor.GOLD + "You have poked " + ChatColor.AQUA + target.getName() + ChatColor.GOLD + "!");
-			   
-			   target.playSound(target.getLocation(), Sound.NOTE_PLING, 10, 1);
-			   target.sendMessage(ChatColor.AQUA + sender.getName() + ChatColor.GOLD + " has poked you!");
-		
-			   return true;
-			   
-			   //End of poke command!
+			
+			if (target == null) {
+				sender.sendMessage(ChatColor.GOLD + "Could not find player");
+				return true;
+			}
+			if (target == sender) {
+				sender.sendMessage(ChatColor.RED + "You can not poke yourself!");
+				return true;
+			}
+			
+			sender.sendMessage(ChatColor.GOLD + "You have poked " + ChatColor.AQUA + target.getName() + ChatColor.GOLD + "!");
+  
+			target.playSound(target.getLocation(), Sound.NOTE_PLING, 10, 1);
+			target.sendMessage(ChatColor.AQUA + sender.getName() + ChatColor.GOLD + " has poked you!");
+		return true;
+		//End of poke command!
 		}
 		return true;
 	}
