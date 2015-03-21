ae9347e12ccf9d4eee0f2c8b2f7db33ebaffa77c
Altra Developer Farmer <altradev@hotmail.com>
2015-03-21 00:33:49 -0400

    Added the Poke Command

--- Craficity/src/com/AltraDev/Crafticity/Crafticity.java
+++ Craficity/src/com/AltraDev/Crafticity/Crafticity.java
@@ -1,12 +1,14 @@
 package com.AltraDev.Crafticity;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.AltraDev.Crafticity.References;
 
+import com.AltraDev.Crafticity.References;
 import com.AltraDev.Crafticity.Listeners;
 
 public class Crafticity extends JavaPlugin {
@@ -21,8 +23,33 @@ public class Crafticity extends JavaPlugin {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		Player player = (Player) sender;
+		// Start of Poke Command
+		if (cmd.getName().equalsIgnoreCase("poke")) {
+			   if (args.length == 0) {
+				  
+			   sender.sendMessage(ChatColor.GOLD + "Use this command to poke someone!");
+			   }
+			   
+			   @SuppressWarnings("deprecation")
+			Player target = Bukkit.getServer().getPlayer(args[0]);
+			   
+			   if (target == null) {
+			    sender.sendMessage(ChatColor.GOLD + "Could not find player");
+			    return true;
+			   }
+			   if (target == sender) {
+				   sender.sendMessage(ChatColor.RED + "You can not poke yourself!");
+				   return true;
+			   }
+			   sender.sendMessage(ChatColor.GOLD + "You have poked " + ChatColor.AQUA + target.getName() + ChatColor.GOLD + "!");
+			   
+			   target.playSound(target.getLocation(), Sound.NOTE_PLING, 10, 1);
+			   target.sendMessage(ChatColor.AQUA + sender.getName() + ChatColor.GOLD + " has poked you!");
 		
-		
+			   return true;
+			   
+			   //End of poke command
+		}
 		return true;
 	}
 }
